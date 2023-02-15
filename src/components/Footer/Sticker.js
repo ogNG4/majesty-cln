3881e5495c3d699f7de668c7c2f6626857cc0a04
OGxNG4 <ogxng4@frontend.com>
2023-02-15 10:18:49 +0100

    sitcker modal fix

--- src/components/Footer/Sticker.js
+++ src/components/Footer/Sticker.js
@@ -1,17 +1,21 @@
+import { FaTimes} from 'react-icons/fa'
+
 import css from "./Sticker.module.css";
 import StickerModal from "../UI/StickerModal";
 const Sticker = (props) => {
   return (
     <StickerModal onClose={props.onClose}>
       <div className={css.over} onClick={props.onClose}>
-        
+        <button className={css.close} onClose={props.onClose}>
+          <FaTimes/>
+        </button>
         <h3>DOSTANIESZ JEŚLI CHCESZ!</h3>
         <p>
           Podeślij zaadresowaną do siebie kopertę zwrotną wraz ze znaczkiem
           pocztowym, tak abyśmy mogli ją do ciebie odesłać. Listy wraz z kopertą
           i znaczkiem prosimy przesłać na adres:
         </p>
-        <p>Majesty Skis ul. T. Wendy 15 81-341 Gdynia</p>
+        <p className={css.adres}>Majesty Skis <br></br>ul. T. Wendy 15<br></br> 81-341 Gdynia</p>
         <p>Poczekaj kilka dni i sprawdź w swojej skrzynce pocztowej. Naklejki powinny już być!</p>
       </div>
     </StickerModal>
